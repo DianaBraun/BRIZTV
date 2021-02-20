13395a929e8018539b179cfc88da41af2cf53f11
Diana Braun <braundiana8@gmail.com>
2021-02-20 12:08:44 +0200

    briztv

---
+++ cypress/integration/Briztests/FooterTest.spec.js
@@ -0,0 +1,9 @@
+describe("Footer Units", function() {
+    it("Add a channel to favorite", function() {
+        cy.visit("https://ottdevfront.briz.ua/")
+        cy.contains("Увійти").click()
+        cy.get("input[type='text']").type("braundiana8@gmail.com")
+        cy.get("input[type='password']").type("19111994q")
+        cy.get("button[type='submit']").click()
+    })
+})
